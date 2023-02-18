ff62b51dbc50908ca81505ff8f722c73755bcf1d
allison-baker <baker.allison.m@gmail.com>
2023-02-17 20:55:58 -0700

    responsive logo, responsive nav

--- 05/js/scripts.js
+++ 05/js/scripts.js
@@ -1,3 +1,7 @@
 // Inject current year in footer
 const currYear = new Date();
 document.querySelector('#year').textContent = currYear.getFullYear();
+
+function toggleNav() {
+    document.querySelector('#primaryNav').classList.toggle('open');
+}
