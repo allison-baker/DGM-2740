dbd61169e1c02aa5ba46e096be79a7f9dbbf1bde
allison-baker <baker.allison.m@gmail.com>
2023-03-04 22:16:22 -0700

    style footer, nav, header

--- 07/js/scripts.js
+++ 07/js/scripts.js
@@ -1,3 +1,8 @@
 // Inject current year into footer
 const currYear = new Date();
 document.querySelector('#year').textContent = currYear.getFullYear();
+
+function toggleNav() {
+    document.querySelector('#hamburgerButton').classList.toggle('open');
+    document.querySelector('#primaryNav').classList.toggle('open');
+}
