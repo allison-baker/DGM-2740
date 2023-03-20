24506cb64caf29cf620f03448a90812bba5563b6
allison-baker <baker.allison.m@gmail.com>
2023-03-20 12:03:01 -0600

    finish nav

--- 08/js/scripts.js
+++ 08/js/scripts.js
@@ -1,2 +1,10 @@
 const currYear = new Date();
 document.querySelector('#year').textContent = currYear.getFullYear();
+
+const hb = document.querySelector('#hamburgerButton');
+const pn = document.querySelector('#primaryNav');
+
+hb.addEventListener('click', () => {
+    hb.classList.toggle('open');
+    pn.classList.toggle('open');
+})
