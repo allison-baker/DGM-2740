44ca393aa9fe754eae3cbee235004113b0c5befa
allison-baker <baker.allison.m@gmail.com>
2023-03-23 11:56:32 -0600

    add slider and favicon, start styling

--- 09/js/scripts.js
+++ 09/js/scripts.js
@@ -1,2 +1,17 @@
 const currYear = new Date();
 document.querySelector('#year').textContent = currYear.getFullYear();
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
