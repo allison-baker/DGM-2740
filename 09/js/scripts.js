ace1c6086b36c5bda0e047eee2785627d0d84dd2
allison-baker <baker.allison.m@gmail.com>
2023-03-28 16:43:08 -0600

    finish nav

--- 09/js/scripts.js
+++ 09/js/scripts.js
@@ -1,17 +1,23 @@
 const currYear = new Date();
-document.querySelector('#year').textContent = currYear.getFullYear();
+document.querySelector("#year").textContent = currYear.getFullYear();
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 5000,
-    },
-    speed: 2000,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  });
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 2000,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const hb = document.querySelector('#hamburgerButton');
+hb.addEventListener('click', () => {
+  document.querySelector('#primaryNav').classList.toggle('open');
+  hb.classList.toggle('open');
+})
