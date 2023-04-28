4af12a92b9430e5e6730b4ecfe9393d81ab94b77
allison-baker <baker.allison.m@gmail.com>
2023-04-28 16:59:14 -0600

    add images, finish home and donate pages

---
+++ capstone/js/scripts.js
@@ -0,0 +1,19 @@
+function toggleNav() {
+  document.querySelector("#hamburgerBtn").classList.toggle("open");
+  document.querySelector("#primaryNav").classList.toggle("open");
+}
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 2000,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
